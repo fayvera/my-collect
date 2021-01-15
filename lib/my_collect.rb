8fc202fd24389058b72ef509377a90dd2a42b01c
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-14 20:51:58 -0500

    commit tree bb84d0cd2bd151c33450cbb551cecd529691c958

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
+
+def my_collect(collection)
+   i = 0
+   new_array = []
+   while i < collection.length
+    yield(collection[i])
+    new_array << yield(collection[i])
+    i += 1
+   end
+   new_array
+end
+
+
 
 
